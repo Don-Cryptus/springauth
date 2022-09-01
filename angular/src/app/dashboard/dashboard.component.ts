ea880eab4db90bb1a9ed93fbdcba7122c886b700
Don-Cryptus <don.cryptus@gmail.com>
2022-09-01 21:23:59 +0200

    clean up

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(public dashboard: DashboardService) {}
 
   ngOnInit(): void {
+    this.dashboard.all().subscribe({
+      next(value) {
+        console.log(value);
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
   }
-
 }
