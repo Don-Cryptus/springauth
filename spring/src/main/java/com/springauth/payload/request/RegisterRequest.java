26f9636f4a079576c242b6f3132bc63cd880fad2
Don-Cryptus <don.cryptus@gmail.com>
2022-10-22 22:10:30 +0200

    add new test

--- spring/src/main/java/com/springauth/payload/request/RegisterRequest.java
+++ spring/src/main/java/com/springauth/payload/request/RegisterRequest.java
@@ -1,10 +1,14 @@
 package com.springauth.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
+@AllArgsConstructor
 public class RegisterRequest {
     @NotBlank
     @Size(min = 3, max = 20)
--- spring/src/test/java/com/springauth/controller/AuthControllerTest.java
+++ spring/src/test/java/com/springauth/controller/AuthControllerTest.java
@@ -1,14 +1,19 @@
 package com.springauth.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springauth.entity.User;
 import com.springauth.payload.request.LoginRequest;
+import com.springauth.payload.request.RegisterRequest;
+import com.springauth.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -16,20 +21,71 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthControllerTest {
 
+    private final String username = "test";
+    private final String password = "testtest";
+    private final String email = "test@test.test";
+    @Autowired
+    PasswordEncoder encoder;
     @Autowired
     ObjectMapper objectMapper;
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeAll
+    public void setup() {
+        User user = new User(username, email, encoder.encode(password));
+        userRepository.save(user);
+    }
+
+    @Test
+    void registerUser() throws Exception {
+
+        RegisterRequest registerRequest = new RegisterRequest("test1", "test1@test.test", password);
+        String registerRequestAsString = objectMapper.writeValueAsString(registerRequest);
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registerRequestAsString)
+                        .characterEncoding("utf-8"))
+                .andExpectAll(
+                        status().isOk(),
+                        MockMvcResultMatchers.jsonPath("$.message").value("User registered successfully!")
+                )
+                .andReturn();
+    }
+
+    @Test
+    void registerUser_alreadyExist() throws Exception {
+
+        RegisterRequest registerRequest = new RegisterRequest(email, password, password);
+        String registerRequestAsString = objectMapper.writeValueAsString(registerRequest);
+
+        var result = mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registerRequestAsString)
+                        .characterEncoding("utf-8"))
+                .andExpectAll(
+                        status().isOk(),
+                        MockMvcResultMatchers.jsonPath("$.message").value("User registered successfully!")
+                )
+                .andReturn();
+
+        System.out.println(111111);
+        System.out.println(result.getResponse().getContentAsString());
+        System.out.println(111111);
+    }
 
     @Test
     void loginUser() throws Exception {
-
-        LoginRequest loginRequest = new LoginRequest("test", "zS5HFXTFxYJsow4k");
+        LoginRequest loginRequest = new LoginRequest(username, password);
         String loginRequestAsString = objectMapper.writeValueAsString(loginRequest);
 
-        MvcResult result = mockMvc.perform(post("/api/auth/login")
+        mockMvc.perform(post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(loginRequestAsString)
                         .characterEncoding("utf-8"))
@@ -39,6 +95,5 @@ class AuthControllerTest {
                 )
                 .andReturn();
 
-        System.out.println(result.getResponse().getContentAsString());
     }
 }
