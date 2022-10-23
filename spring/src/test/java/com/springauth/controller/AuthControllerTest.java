a6d22c8c1d6bd16fe5aab19fd3f176ae24f40ecf
Don-Cryptus <don.cryptus@gmail.com>
2022-10-23 22:13:01 +0200

    clean up tests

--- spring/src/test/java/com/springauth/controller/AuthControllerTest.java
+++ spring/src/test/java/com/springauth/controller/AuthControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class AuthControllerTest {
 
     @Autowired
--- spring/src/test/java/com/springauth/controller/DashboardControllerTest.java
+++ spring/src/test/java/com/springauth/controller/DashboardControllerTest.java
@@ -9,17 +9,21 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.springauth.testdata.UserTestData.*;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class DashboardControllerTest {
     @Autowired
     PasswordEncoder encoder;
@@ -37,18 +41,21 @@ class DashboardControllerTest {
         userRepository.save(new User(USERNAME, EMAIL, encoder.encode(PASSWORD)));
     }
 
-//    @Test
-//    void userAccess() throws Exception {
-//        String token = jwtUtils.generateTokenFromUsername(USERNAME);
-//
-//        mockMvc.perform(get("/api/dashboard/user")
-//                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
-//                .andExpectAll(
-//                        status().isOk(),
-//                        content().string("User Content.")
-//                )
-//                .andReturn();
-//    }
+    @Test
+    void userAccess() throws Exception {
+        System.out.println(userRepository.findAll().size());
+        System.out.println(userRepository.findAll().size());
+        System.out.println(userRepository.findAll().size());
+        String token = jwtUtils.generateTokenFromUsername(USERNAME);
+
+        mockMvc.perform(get("/api/dashboard/user")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .andExpectAll(
+                        status().isOk(),
+                        content().string("User Content.")
+                )
+                .andReturn();
+    }
 
     @Test
     void userAccess_noToken() throws Exception {
